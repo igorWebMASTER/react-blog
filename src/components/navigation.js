0e318e3aad9392e41131c08fd18e020a411ea5d4
igorWebMASTER <igor_1917@hotmail.com>
2020-09-19 13:33:06 -0300

    Navigation done

---
+++ src/components/navigation.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Avatar } from "antd";
+
+const navLinks = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+  {
+    title: "Contact Us",
+    path: "/contact-us",
+  },
+  {
+    title: "Login",
+    path: "/login",
+  },
+];
+
+export default function Navigation({ user }) {
+  const [menuActive, setMenuActive] = useState(false);
+
+  return (
+    <nav className="site-navigation">
+      <span className="menu-title">Meu Blog</span>
+      <div className="menu-content-container">
+        <ul>
+          {navLinks.map((link, index) => (
+            <li key={index}>
+              <Link to={link.path}>{link.title}</Link>
+            </li>
+          ))}
+        </ul>
+        <span>
+          {" "}
+          <Avatar
+            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+            size={38}
+          />
+          <span className="menu-avatar-name">{`${user.firstName} ${user.lastName}`}</span>
+        </span>
+        <i className="ionicons icon ion-ios-menu"></i>
+      </div>
+    </nav>
+  );
+}
---
+++ src/pages/contact-us.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function ContactUs() {
+  return <div>Contact Us</div>;
+}
+
+export default ContactUs;
