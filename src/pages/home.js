13daf5a90244caee1a28a14c4b22f2158c4d9eff
igorWebMASTER <igor_1917@hotmail.com>
2020-09-23 23:58:39 -0300

    pages created

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,17 @@
 import React from "react";
-
+import { PostMasonry } from ".../component/commom";
+import trending from "../assets/mocks/trending";
 // import { Container } from './styles';
 
 function Home() {
-  return <section className="container home">Home</section>;
+  return (
+    <section className="container home">
+      <div className="row">
+        <h2>Trending Posts</h2>
+        <PostMasonry posts={trending} columns={3} />
+      </div>
+    </section>
+  );
 }
 
 export default Home;
