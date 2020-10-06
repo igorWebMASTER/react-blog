ebd60afe6f7f9a5730e6455814107e56f9a23c48
igorWebMASTER <igor_1917@hotmail.com>
2020-10-05 23:18:59 -0300

    ajuste no grid de posts

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PostMasonry, MasoryPost } from '../components/common';
+import { PostMasonry } from '../components/common';
 import trending from '../assets/mocks/trending';
 import featured from '../assets/mocks/featured';
 
@@ -20,8 +20,6 @@ const mergeStyles = function (posts, config) {
 mergeStyles(trending, trendingConfig);
 mergeStyles(featured, featuredConfig);
 
-const lastFeatured = featured.pop();
-
 export default function Home() {
   return (
     <section className='container home'>
@@ -29,7 +27,6 @@ export default function Home() {
         <h1>Featured Posts</h1>
         <section className='featured-posts-container'>
           <PostMasonry posts={featured} columns={2} tagsOnTop={true} />
-          <MasoryPost posts={lastFeatured} tagsOnTop={true} columns={3} />
         </section>
         <h1>Posts mais populares</h1>
         <PostMasonry posts={trending} columns={3} />
